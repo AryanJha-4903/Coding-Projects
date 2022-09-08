d2251b2f881678aa809bd142e1cb7de4d88d6f0b
AryanJha-4903 <aryaj49btechcse2025@kccitm.edu.in>
2022-09-08 09:03:49 +0530

    cbfhgf

--- student and mentor google kickstart.cpp
+++ student and mentor google kickstart.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-int mentor(int n,int *arr)
+void mentor(int n,int *arr)
 {
-    
+    for (int i = 0; i <=n-1; i++)
+    {
+        int temp= -1;
+        for (int j = 0; j <=n-1; j++)
+        {
+            if (arr[j]>temp&&arr[j]<=2*arr[i]&&i!=j)
+            {
+                temp=arr[j];
+            }
+        }
+        cout<<temp<<' ';
+    }
+}
+int main()
+{
+    int test;
+    cin>> test;
+    for (int i = 1; i <=test; i++)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int j = 0; j <=n-1; j++)
+        {
+            cin>>arr[j];
+        }
+        cout<<"Case"<<" #"<<i<<" : ";
+        mentor(n,arr);
+        cout<<endl;
+    }
 }
