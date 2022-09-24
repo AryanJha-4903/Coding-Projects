7bcdbd974f7d1643078d7b2e7a39b68a40bbf916
Arya Koutilya  Jha <107873600+AryanJha-4903@users.noreply.github.com>
2022-09-24 21:25:50 +0530

    Add files via upload

---
+++ assignment 2/sum and difference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a;
+    float b;
+    scanf("%f %f",&a,&b);
+    printf("%0.1f %0.1f",a+b,a-b);
+    return 0;
+}
