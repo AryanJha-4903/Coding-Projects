7e7a447a7d38a34753d5981dd558be1dba76f4f1
Arya Koutilya  Jha <107873600+AryanJha-4903@users.noreply.github.com>
2022-10-02 22:15:14 +0530

    Add files via upload

--- save the prisoner hackerrank.py
+++ save the prisoner hackerrank.py
@@ -2,9 +2,12 @@ import math
 #main functon
 def prisoner(n, t, s):
     x=t%n+s-1
-    return x
+    if x==0:
+        return n
+    else:
+        return x
 #driver code
 for i in range (int(input())):
     n, t, s = [int(x) for x in input().split()]
     print(prisoner(n, t, s))
-#end1
+#end
