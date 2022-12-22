979e9d13d4e8c6a827476cdcfa9f11d0bde1eab4
Arya Koutilya  Jha <107873600+AryanJha-4903@users.noreply.github.com>
2022-12-22 21:43:25 +0530

    Create sqrt.cpp

---
+++ assignment 7/sqrt.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin>>x;
+    int ans=round(pow(x,0.5));
+    cout<<ans;
+    return 0;
+}
